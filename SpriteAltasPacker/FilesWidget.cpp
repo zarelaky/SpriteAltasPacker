5fdcbae8db551dda6ae03b8b64cb671107e5b722
zarelaky <zarelaky@gmail.com>
2013-12-11 16:10:18 +0800

    file view drag drop

--- SpriteAltasPacker/FilesWidget.cpp
+++ SpriteAltasPacker/FilesWidget.cpp
@@ -1,14 +1,97 @@
+#include <QMessageBox>
+#include <QUrl>
+#include <QMimeData>
+#include <QString>
+#include <QDir>
+
 #include "FilesWidget.h"
 #include "ui_filesview.h"
-
 
 FilesWidget::FilesWidget(QWidget* p)
 	: QDockWidget(p)
 {
-    Ui::FilesDockWidget* ui = new Ui::FilesDockWidget();
-	ui->setupUi(this);
+    _ui = new Ui::FilesDockWidget();
+	_ui->setupUi(this);
+    setAcceptDrops(true);
 }
 
 FilesWidget::~FilesWidget()
 {
+}
+
+void FilesWidget::dragEnterEvent(QDragEnterEvent* evt)
+{
+    if (evt->mimeData()->hasUrls()) {
+        evt->acceptProposedAction();
+    }
+} 
+void FilesWidget::dropEvent(QDropEvent* evt)
+{
+    if (evt->mimeData()->hasUrls()) {
+        foreach(QUrl url, evt->mimeData()->urls()) {
+            if (url.isLocalFile()){
+                QMessageBox::information(NULL, "info", url.toLocalFile());
+                addToTree(url.toLocalFile());
+            }
+        }
+    }
+}
+
+void FilesWidget::addToTree(const QString& file)
+{
+    QFileInfo fi(file);
+    if (fi.isDir()){
+        QMessageBox::information(NULL, "info", "bbb");
+        addDir(fi);
+        return;
+    }
+    addFile(fi);
+    QMessageBox::information(NULL, "info", "aaa");
+
+    _ui->treeWidget->expandAll();
+}
+
+void FilesWidget::addDir(const QFileInfo& fi, QTreeWidgetItem* p)
+{
+    QTreeWidget* tw = _ui->treeWidget;
+    QTreeWidgetItem* item = NULL;
+    if (NULL == p) {
+        item = new QTreeWidgetItem(tw);
+        tw->addTopLevelItem(item);
+    } else {
+        item = new QTreeWidgetItem(p);
+    }
+    item->setText(0, fi.fileName());
+
+    QDir dir(fi.absoluteFilePath());
+
+    QFileInfoList fil = dir.entryInfoList(QDir::AllEntries|QDir::Readable|QDir::NoDotAndDotDot);
+    typedef QListIterator<QFileInfo> QFileInfoListIterator;
+    QFileInfoListIterator i(fil);
+  //  QMessageBox::information(this, "", QString("%1 %2").arg(dir.dirName()).arg(fil.size()));  
+    while (i.hasNext()){
+        QFileInfo inf = i.next();        
+       if (inf.isDir()) {
+           addDir(inf, item);
+       } else if (inf.isFile()) {
+           addFile(inf, item);
+       }
+    }
+    
+}
+
+void FilesWidget::addFile(const QFileInfo& fi, QTreeWidgetItem* p)
+{
+    QTreeWidget* tw = _ui->treeWidget;
+    QTreeWidgetItem* item = NULL;
+    int level = 0;
+    if (NULL == p) {
+        item = new QTreeWidgetItem(tw);
+        tw->addTopLevelItem(item);    
+    } else {
+    //    level  = 1;
+        item = new QTreeWidgetItem(p);
+    }
+    item->setText(level, fi.fileName());
+
 }
--- SpriteAltasPacker/FilesWidget.h
+++ SpriteAltasPacker/FilesWidget.h
@@ -1,12 +1,27 @@
 #pragma once
 #include <QDockWidget>
+#include <QDragEnterEvent>
+#include <QDropEvent>
+#include <QFileInfo>
+#include <QTreeWidgetItem>
 
+namespace Ui
+{
+    class FilesDockWidget;
+}
 class FilesWidget : public QDockWidget
 {
 	Q_OBJECT;
 public:
 	FilesWidget(QWidget* parent);
 	~FilesWidget();
-private:
 
+    void dragEnterEvent(QDragEnterEvent* evt);
+    void dropEvent(QDropEvent* e);
+protected:
+    void addToTree(const QString& file);
+    void addDir(const QFileInfo& fi, QTreeWidgetItem* parent= NULL);
+    void addFile(const QFileInfo& fi, QTreeWidgetItem* parent = NULL);
+private:
+    Ui::FilesDockWidget* _ui;    
 };
